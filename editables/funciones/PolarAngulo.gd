fb61b4acbbeb1ba1fea7bf89110f498e31ad0e72
Rodrigo René Cura <rodrigo.renecura@gmail.com>
2021-09-21 11:14:52 -0300

    Level ejemplo con boss

--- editables/funciones/PolarAngulo.gd
+++ editables/funciones/PolarAngulo.gd
@@ -1,8 +1,12 @@
 extends FuncionBase
 class_name AA_PolarAngulo
 
-export var speed: float = 0.0
-export var magnitud: float = 100.0
+export var initial_angle: float = -90
+export var angular_speed: float = 0.0
+export var bullet_speed: float = 100.0
 
 func value(t: float) -> Vector2:
-	return polar2cartesian(magnitud, deg2rad(-speed * t))
+	return polar2cartesian(
+		bullet_speed,
+		deg2rad(initial_angle) - angular_speed * t
+		)
